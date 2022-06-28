bc56c62b77170950fb8793b91f30e49017b6ca84
Andson <andsonsilva2015@hotmail.com>
2022-06-28 17:55:46 -0300

    finished

---
+++ FRONT-END/JavaScript/TypeScriptParte1-EvoluindoSeuJavaScript/app/models/negociacao.ts
@@ -0,0 +1,15 @@
+export class Negociacao {
+    
+
+    constructor(
+        public readonly _data: Date,
+        public readonly _quantidade: number,
+        public readonly _valor: number
+    ) {
+        
+    }
+
+    get volume(): number {
+        return this._valor * this._quantidade
+    }
+}
